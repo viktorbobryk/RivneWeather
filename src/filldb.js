f8dfbfca1493b0941e9b9fceb4ec86a8f08f9be0
Viktor <Viktor>
2016-11-06 14:37:30 +0200

    fix mistakes

--- src/filldb.js
+++ src/filldb.js
@@ -1,11 +1,11 @@
 'use strict';
- console.log("filldb works ");
+console.log("filldb works ");
 var request = require('request');
 var Logger = require('./services/logger.js');
 var MongoClient = require('mongodb').MongoClient;
 
 var url = 'https://api.darksky.net/forecast/73ca2db6aa635b831a24746659e7c907/50.37,26.15';
-var urlDB = 'mongodb://localhost:27017/weather2';
+var urlDB = 'mongodb://localhost:27017/weather';
 
 var logger = new Logger('./logs/log.txt', false);
 
@@ -24,8 +24,7 @@ setInterval(function () {
                 }
 
 
-                var collection = db.collection('weather');
-                
+                var collection = db.collection('weather3');
                 collection.insertOne(JSON.parse(body).main, function (error, result) {
                     if (error) {
                         logger.logError(error);
@@ -33,7 +32,6 @@ setInterval(function () {
                     console.log(result);
                     console.log("----------------------------");
                 });
-                
                 db.close();
             });
         }
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 'use strict';
- console.log("server works ");
+console.log("server works ");
 var express = require('express');
 var bodyParser = require("body-parser");
 var MongoClient = require('mongodb').MongoClient;
@@ -10,11 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.get('/data', function (req, res) {
-    MongoClient.connect('mongodb://localhost:27017/weather2', function (error, db) {
+    MongoClient.connect('mongodb://localhost:27017/weather', function (error, db) {
         if (error) {
             console.log("connection error " + error);
         }
-         var collection = db.collection('weather2');
+         var collection = db.collection('weather3');
       
          collection.find().toArray(function (err, docs) {
             if (err) {
